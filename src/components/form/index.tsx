b59436099264b849d738cac211ab4015fecb1af8
Nando <nandosilva98@outlook.com>
2023-04-24 20:42:03 -0300

    add custom info to form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,29 +1,32 @@
-function Form() {
+interface FormType {
+  title: string;
+  seconds: string;
+  average: string;
+  time_remaining: string;
+}
+
+function Form(props: FormType) {
   return (
     <>
       <div className="bg-purple-900 max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl opacity-75">
-        <h1 className="text-center text-2xl">Calculadora de Aço Negro</h1>
+        <h1 className="text-center text-2xl">{props.title}</h1>
         <div className="relative mb-3 text-center px-16 mx-1">
-          <label className="pt-10 mb-5 block">
-            Em Quantos Segundos Você Minera?
-          </label>
+          <label className="pt-10 mb-5 block">{props.seconds}</label>
           <input
             type="number"
-            className="bg-gray-600 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             placeholder="EX: 8"
           />
-          <label className="pt-10 mb-5 block">
-            Media de Dark Steel Minerado por Vez?
-          </label>
+          <label className="pt-10 mb-5 block">{props.average}</label>
           <input
             type="number"
-            className="bg-gray-600 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             placeholder="EX: 110"
           />
-          <label className="pt-10 mb-5 block">Tempo Restante em Minuto?</label>
+          <label className="pt-10 mb-5 block">{props.time_remaining}</label>
           <input
             type="number"
-            className="bg-gray-600 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             placeholder="EX: 50"
           />
           <br />
@@ -40,4 +43,94 @@ function Form() {
   );
 }
 
-export default Form;
+interface XPType {
+  title: string;
+  porcem: string; 
+  level: string;
+}
+
+function Exp(props: XPType) {
+  return (
+    <div>
+      <div className="bg-purple-900 max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl opacity-75">
+        <h1 className="text-center text-2xl">{props.title}</h1>
+        <div className="relative mb-3 text-center px-16 mx-1">
+          <label className="pt-10 mb-5 block">{props.porcem}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 8"
+          />
+          <label className="pt-10 mb-5 block">{props.level}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 110"
+          />
+          <br />
+          <br />
+          <button
+            type="button"
+            className="rounded bg-blue-600 hover:bg-blue-800 px-6 pb-3 pt-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          >
+            Calcular
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface FarmType {
+  title: string;
+  levelup: string;
+  mob_kill: string;
+  mob_time: string;
+  time_remaining: string;
+}
+
+function Farm(props: FarmType) {
+  return (
+    <>
+        <div className="bg-purple-900 max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl opacity-75">
+        <h1 className="text-center text-2xl">{props.title}</h1>
+        <div className="relative mb-3 text-center px-16 mx-1">
+          <label className="pt-10 mb-5 block">{props.levelup}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 8"
+          />
+          <label className="pt-10 mb-5 block">{props.mob_kill}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 110"
+          />
+          <label className="pt-10 mb-5 block">{props.mob_time}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 50"
+          />
+          <label className="pt-10 mb-5 block">{props.time_remaining}</label>
+          <input
+            type="number"
+            className="bg-gray-800 peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            placeholder="EX: 50"
+          />
+          <br />
+          <br />
+          <button
+            type="button"
+            className="rounded bg-blue-600 hover:bg-blue-800 px-6 pb-3 pt-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          >
+            Calcular
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export { Form, Exp, Farm };
--- src/pages/Experiencia.tsx
+++ src/pages/Experiencia.tsx
@@ -1,13 +1,26 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Form from "./../components/form/index";
+import { Exp, Farm } from "./../components/form/index";
 
 function Experiencia() {
   return (
     <div>
       <div className='h-screen bg-[url("https://raw.githubusercontent.com/nandonweb/Mir4/main/images/calculadora/fundo_exp.png")] bg-no-repeat bg-cover '>
         <Header></Header>
-        <Form></Form>
+        <div className="flex justify-around mx-28">
+        <Exp
+          title="Calculadora de XP"
+          porcem="Seus 5% de XP?"
+          level="Porcentagem do Level Atual"
+        />
+        <Farm 
+          title="Calculadora de Farm"
+          levelup="XP Necessario para Upar de Level?"
+          mob_kill="XP por cada monstro abatido?"
+          mob_time="Quantos Segundos para abater um mob?" 
+          time_remaining="Tempo de Farm?"        
+        />
+        </div>
         <Footer></Footer>
       </div>
     </div>
